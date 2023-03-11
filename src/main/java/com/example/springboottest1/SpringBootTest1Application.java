1fcc8c4367b6e2211193ad6eb85e65c79d5243a8
seokjoon <seokjoon@gmail.com>
2023-03-11 13:36:39 +0900

    ignore

--- src/main/java/com/example/springboottest1/SpringBootTest1Application.java
+++ src/main/java/com/example/springboottest1/SpringBootTest1Application.java
@@ -2,12 +2,13 @@ package com.example.springboottest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringBootTest1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTest1Application.class, args);
     }
-
 }
